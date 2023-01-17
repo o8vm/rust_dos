24a8388e11f1f5daa7cfb2bb36c6b9391e071b61
Edwin Amsler <EdwinGuy@GMail.com>
2023-01-16 21:57:17 -0600

    Implement date and time getting/setting
    References:
    * http://spike.scu.edu.au/~barry/interrupts.html
    * https://www.cs.uaf.edu/2017/fall/cs301/lecture/09_11_registers.html

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod dos_tests;
 
 use rust_dos::*;
 use crate::dos_tests::allocator_test::allocator_test;
+use crate::dos_tests::datetime::datetime_test;
 use crate::dos_tests::file::file_read_test;
 
 entry!(main);
@@ -15,4 +16,5 @@ entry!(main);
 fn main() {
     allocator_test();
     file_read_test();
+    datetime_test();
 }
