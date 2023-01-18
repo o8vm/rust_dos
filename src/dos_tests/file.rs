7b1628e79dcd7b9df8df21a7dd642bb9fd6f4491
Edwin Amsler <EdwinGuy@GMail.com>
2023-01-17 23:28:13 -0600

    Add file attribute reading

--- src/dos_tests/file.rs
+++ src/dos_tests/file.rs
@@ -11,17 +11,15 @@ pub(crate) fn file_read_test() {
         Ok(_) => println!("File closed"),
         Err(_) => println!("Error closing file")
     }
+}
 
-    let test_file = dos::file::File::open("C:\\AUTOEXEC.BAT").unwrap();
-    let mut buffer = [0; 100];
-    let bytes_read = test_file.read(&mut buffer).unwrap();
-    println!("{} bytes read", bytes_read);
-    println!("{}", core::str::from_utf8(&buffer).unwrap());
+#[allow(dead_code)]
+pub(crate) fn file_attribute_test() {
+    let attributes = dos::file::File::attributes("C:\\AUTOEXEC.BAT");
+    let attributes = attributes.unwrap_or(dos::file::File::attributes("README.md").unwrap());
 
-    match test_file.close() {
-        Ok(_) => println!("File closed"),
-        Err(_) => println!("Error closing file")
-    }
+    // Attributes aren't supported in DOSBox so expect this to have no info
+    println!("Attributes: {:?}", attributes);
 
-    println!("Hello, World!");
+    println!("Long filename {:?}", dos::file::File::attributes("Really long name or something"));
 }
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ mod dos_tests;
 use rust_dos::*;
 use crate::dos_tests::allocator_test::allocator_test;
 use crate::dos_tests::file::file_read_test;
+use crate::dos_tests::file::file_attribute_test;
 
 entry!(main);
 
 fn main() {
     allocator_test();
     file_read_test();
+    file_attribute_test();
 }
